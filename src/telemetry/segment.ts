fef6767843b2a7bb2b10ca053ef21a3daca6690c
Duane Adam <hello@duaneadam.me>
2026-02-10 13:38:03 +0800

    chore: release 10.0.0

--- src/telemetry/segment.ts
+++ src/telemetry/segment.ts
@@ -372,7 +372,7 @@ function withTypewriterContext<P extends Record<string, any>, T extends TrackMes
             ...(message.context || {}),
             typewriter: {
                 language: 'typescript',
-                version: '9.2.0',
+                version: '10.0.0',
             },
         },
     }
@@ -647,3 +647,4 @@ export default new Proxy<typeof clientAPI>(clientAPI, {
         };
     },
 });
+
